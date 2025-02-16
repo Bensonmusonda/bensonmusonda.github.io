8709218d009054bc35dd69aab3d3ff143aa2ed86
Benson Musonda <70434034+Bensonmusonda@users.noreply.github.com>
2025-02-16 19:25:28 +0200

    add javascript to fetch and display user data on profile page

---
+++ bennieslab/bennieslab/src/test/java/com/bennieslab/portfolio/BennieslabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bennieslab.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BennieslabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
